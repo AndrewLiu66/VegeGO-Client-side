aa801424f60c58419d6d13e9e399d7fce62acd9c
AndrewLiu66 <zhaoyu.liu@hotmail.com>
2021-12-05 18:42:21 -0800

    begin

--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -24,8 +24,8 @@ const useNearbyListEffect = () => {
   const getNearbyList = async () => {
     const result = await get("/api/shop/hot-list");
     //?.的作用跟.差不多,但是如果没有errono系统不会报错;result?.data?.length表示data里面有数据
-    if (result?.errno === 0 && result?.result?.length) {
-      nearbyList.value = result.result;
+    if (result?.errno === 0 && result?.data?.length) {
+      nearbyList.value = result.data;
     }
   };
   return { nearbyList, getNearbyList };
